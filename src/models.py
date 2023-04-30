91e68c0312e8ff1814ebc504002f27c2a5f086f4
Ben <113471794+CabraCrimes@users.noreply.github.com>
2023-04-30 11:14:16 +0000

    STARWARS DATA MODELING

--- src/models.py
+++ src/models.py
@@ -1,29 +1,43 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class MyUser(Base):
+    __tablename__ = 'my_user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False, unique=True)
+    password = Column(String(250), nullable=False)
+    favourite = relationship('favourites')
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Favourite(Base):
+    __tablename__ = 'favourite'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('my_user.id'), nullable=False)
+    planet_id = Column(String(250), ForeignKey('planet.id'), nullable=False)
+    character_id = Column(String(250), ForeignKey('character.id'), nullable=False)
+    
+   
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), unique=True, nullable=False)
+    population = Column(Integer)
+    favourite = relationship('favourites')
+    
+
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), unique=True, nullable=False)
+    alive = Column(Boolean, nullable=False)
+    favourite = relationship('favourites')
+    
 
     def to_dict(self):
         return {}
